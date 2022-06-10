797395cff7f0d59ae85db639d024462a36c9b82b
Zion Isaiah Prater <zprat5885@gmail.com>
2022-06-09 22:45:43 -0500

    lab4

--- index2.js
+++ index2.js
@@ -1,6 +1,7 @@
 "use strict";
 let name = prompt("What is your name")
 window.alert(name)
+function Func1(){
 let guess = prompt("Was I born in Memphis, Tennessee ?")
 let Answer1 = "No"
 if(Answer1[0]=== Answer1[0].toUpperCase()){
@@ -13,6 +14,9 @@ if (Answer1 == guess) {
   else if (guess == "Yes") {
     alert("Yes is incorrect")
   }
+}
+Func1();
+function Func2(){
   let guess2 = prompt("Did I move to Memphis, Tennessee at the age of four ?")
 let Answer2 = "Yes"
 if(Answer2[0]=== Answer2[0].toUpperCase()){
@@ -25,6 +29,9 @@ if (Answer2 == guess2) {
   else if (guess2 == "No") {
     alert("No is incorrect")
   }
+}
+Func2();
+function Func3(){
   let guess3 = prompt("Is it true that I graduated high school in 2018 ?")
   let Answer3 = "No"
   if(Answer3[0]=== Answer3[0].toUpperCase()){
@@ -37,6 +44,9 @@ if (Answer2 == guess2) {
     else if (guess3 == "Yes") {
       alert("No is incorrect")
     }
+}
+Func3();
+function Func4(){
     let guess4 = prompt("Did I transfer to the University of Tennessee at Chattanooga in 2018 ?")
     let Answer4 = "Yes"
     if(Answer4[0]=== Answer4[0].toUpperCase()){
@@ -49,6 +59,9 @@ if (Answer2 == guess2) {
       else if (guess4 == "No") {
         alert("No is incorrect")
       }
+    }
+    Func4();
+    function Func5(){
       let guess5 = prompt("Was I a delivery driver for Amazon ?")
 let Answer5 = "Yes"
 if(Answer5[0]=== Answer5[0].toUpperCase()){
@@ -61,6 +74,9 @@ if (Answer5 == guess5) {
   else if (guess5 == "No") {
     alert("No is incorrect")
   }
+}
+Func5();
+function Func6(){
   for (let z = 1; z <= 4; z++){
 let guess6 = prompt("Barry Sanders is number _ in my top ten running backs list")
 let Answer6 = "4"
@@ -72,6 +88,9 @@ let Answer6 = "4"
     alert("too low");
   }
 }
+}
+Func6();
+function Func7(){
 for (let z = 1; z <= 6; z++){
     let guess7 = prompt("Can you name one of top two running backs ?")
     let RB =["Walter Payton"|| "Emmitt Smith"]
@@ -85,6 +104,8 @@ for (let z = 1; z <= 6; z++){
     }
     
 }
+}
+Func7();
 
 
 
